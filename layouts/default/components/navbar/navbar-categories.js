4ad6d7067c49d06de447e53bd8b179d9480fe0ca
BasharAbdoulah <des.basharabdulah@gmail.com>
2022-09-17 13:32:32 +0300

    New products APIs added

--- layouts/default/components/navbar/navbar-categories.js
+++ layouts/default/components/navbar/navbar-categories.js
@@ -10,54 +10,6 @@ import IconTitleItem from "components/utils/icon-title-item.js";
 import { COLORS, PADDINGS, STANDARD_SCREENS } from "styles/variables";
 import { useRouter } from "next/router";
 
-// const categories = [
-//     {
-//         name: "Men's Fashion",
-//         name_ar: "أزياء الرجالية",
-//         id: "f6222596-f422-4891-9a55-664e5e3017aa",
-//     },
-//     {
-//         name: "Women's Fashion",
-//         name_ar: "أزياء النسائية",
-//         id: "31afd7d2-5f4d-406d-ae5d-b46e5055b80c",
-//     },
-//     {
-//         name: "Children's Clothing",
-//         name_ar: "ملابس الاطفال",
-//         id: "6956bc2b-b8f6-48be-8be6-6c56983c79d5",
-//     },
-
-//     {
-//         name: "Men's stuff",
-//         name_ar: "مستلزمات الرجال",
-//         id: "bdd1e12a-ffa7-4b9c-93a5-b4180fb33675",
-//     },
-
-//     {
-//         name: "Mobiles & Accessories ",
-//         name_ar: "إكسسوارات و هواتف",
-//         id: "e488e898-4744-4caa-9f12-a676ce5d0a5b",
-//     },
-
-//     {
-//         name: "Accessories and gifts",
-//         name_ar: "الهدايا والإكسسوارات",
-//         id: "d115a1f7-2407-4446-9caa-dc9744e5bfa8",
-//     },
-
-//     {
-//         name: "Home & Kitchen",
-//         name_ar: "المنزل والمطبخ",
-//         id: "d2a67fba-9070-4687-ae75-fb74f25e7924",
-//     },
-
-//     {
-//         name: "Brands",
-//         name_ar: "الماركات",
-//         id: "4774bfdb-5dca-47e7-a43b-0a8acb659305",
-//     },
-// ];
-
 const categories = [
     {
         catNameAR: "موضة نسائية",
